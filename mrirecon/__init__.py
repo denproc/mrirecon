f06f78e9dbc4f26bddeb2a7d7233df9de68f247e
Denis Prokopenko <22414094+denproc@users.noreply.github.com>
2022-04-04 13:09:48 +0100

    feat: fix device

--- mrirecon/__init__.py
+++ mrirecon/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.0.1"
 
 from mrirecon.sense import sense
-from mrirecon.kt_sense import kt_sense
+from mrirecon.kt_sense import kt_sense, slice_wise_kt_sense
 
-__all__ = ['sense', 'kt_sense']
+__all__ = ['sense', 'kt_sense', 'slice_wise_kt_sense']
