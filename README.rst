b62cfa0511b4f719d60550cc7cbbb20de560ad59
Denis Prokopenko <22414094+denproc@users.noreply.github.com>
2023-09-21 18:35:25 +0100

    Fix deployment to PYPI (#2)
    * exclude tests from setup
    
    * fix links

--- README.rst
+++ README.rst
@@ -9,8 +9,8 @@ Our package takes advantage of batched matrix operations, which can be performed
 Available Algorithms
 --------------------
 
-- SENSE reconstruction for parallel imaging (`pdf <https://onlinelibrary.wiley.com/doi/pdfdirect/10.1002/(SICI)1522-2594(199911)42:5%3C952::AID-MRM16%3E3.0.CO;2-S>`_)
-- kt-SENSE reconstruction for dynamic magnetic resonance imaging (MRI) (`pdf <https://onlinelibrary.wiley.com/doi/pdf/10.1002/mrm.10611>`_)
+- SENSE reconstruction for parallel imaging (`pdf <https://onlinelibrary.wiley.com/doi/pdfdirect/10.1002/(SICI)1522-2594(199911)42:5%3C952::AID-MRM16%3E3.0.CO;2-S>`__)
+- kt-SENSE reconstruction for dynamic magnetic resonance imaging (MRI) (`pdf <https://onlinelibrary.wiley.com/doi/pdf/10.1002/mrm.10611>`__)
 
 
 .. installation-section-start
--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from typing import List
 
 def read_version_file(rel_path: str) -> List[str]:
     here = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(here, rel_path), 'r') as fp:
+    with open(os.path.join(here, rel_path), "r") as fp:
         return fp.read().splitlines()
 
 
 def get_version(rel_path: str) -> str:
     for line in read_version_file(rel_path):
-        if line.startswith('__version__'):
+        if line.startswith("__version__"):
             # Example: __version__ = "0.9"
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
@@ -24,10 +24,10 @@ with open("README.rst", "r") as f, open("requirements.txt", "r") as g:
     long_description = f.read()
     required = g.read().splitlines()
 
-package_name = 'mrirecon'
+package_name = "mrirecon"
 setuptools.setup(
     name=package_name,
-    version=get_version(os.path.join(package_name, '__init__.py')),
+    version=get_version(os.path.join(package_name, "__init__.py")),
     author="Denis Prokopenko",
     author_email="d.prokopenko@outlook.com",
     description="MRI reconstruction toolbox. PyTorch.",
@@ -35,7 +35,7 @@ setuptools.setup(
     long_description_content_type="text/x-rst",
     url="https://github.com/denproc/mrirecon",
     install_requires=required,
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
